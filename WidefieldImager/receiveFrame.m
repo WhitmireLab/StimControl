ca92cf0f023d6dfe028151fbbd087f780897bda8
labadmin <labadmin@QBI-8DB6544.uq.edu.au>
2025-04-23 17:38:05 +1000

    wrapping up for the day

--- WidefieldImager/receiveFrame.m
+++
@@ -1,6 +0,0 @@
-function receiveFrame(src, obj)
-    %TODO https://au.mathworks.com/help/imaq/acquire-images-using-parallel-worker.html
-    disp("trigger received!");
-    imgs = getdata(src,src.FramesAvailable);
-    disp("images")
-end
